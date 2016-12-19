6ff434b708212075e90740474887d1d3ca9b126c
Jerems91 <jerem92@free.fr>
2016-12-19 22:41:16 +0100

    19/12/2016
    Correction descriptifs produit

--- JeremsShop/src/main/java/com/jerems91/jeremsshop/modele/Catalogue.java
+++ JeremsShop/src/main/java/com/jerems91/jeremsshop/modele/Catalogue.java
@@ -33,9 +33,9 @@ public class Catalogue implements Serializable {
 		produits.put("2",new Produit("2","Cuisinière",499.95,"Cuisinière","../images/cuisiniere.png","Quelle superbe cuisinière !!!"));
 		produits.put("3",new Produit("3","Machine à café",129.49,"Machine à café","../images/machine-cafe.png","Quelle superbe machine à café !!!"));
 		produits.put("4",new Produit("4","Machine à pain",99.99,"Machine à pain","../images/machine-pain.png","Quelle superbe machine à pain !!!"));
-		produits.put("5",new Produit("5","Micro-ondes",259,"Micro-ondes","../images/micro-ondes.png","Quelle superbe micro-ondes !!!"));
-		produits.put("6",new Produit("6","Shaker",119.99,"Shaker","../images/shaker.png","Quelle superbe shaker !!!"));
-		produits.put("7",new Produit("7","Toaster",129.99,"Toaster","../images/toaster.png","Quelle superbe toaster !!!"));
+		produits.put("5",new Produit("5","Micro-ondes",259,"Micro-ondes","../images/micro-ondes.png","Quel superbe micro-ondes !!!"));
+		produits.put("6",new Produit("6","Shaker",119.99,"Shaker","../images/shaker.png","Quel superbe shaker !!!"));
+		produits.put("7",new Produit("7","Toaster",129.99,"Toaster","../images/toaster.png","Quel superbe toaster !!!"));
 		
 	}
 
